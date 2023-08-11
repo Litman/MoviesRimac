a3f228bf87c9e371e62db3315de6bdd66ecd3727
Litman <litman_ayala@hotmail.com>
2023-08-11 13:29:42 -0500

    Initial Commit

---
+++ MoviesRimac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesRimac
+//
+//  Created by Litman Ayala Laura on 11/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
